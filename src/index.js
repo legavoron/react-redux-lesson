347b793663f6c4a347ad5e2309fff618fd007f52
Aleh Shchurko <alehshchurko@gmail.com>
2022-12-19 21:04:13 +0300

    add connect

--- src/index.js
+++ src/index.js
@@ -1,53 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createStore, bindActionCreators } from 'redux';
-
+import { createStore } from 'redux';
 import reducer from './reducer';
-import * as actions from './actions';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
 
 
 const store = createStore(reducer);
 
-const {dispatch, subscribe, getState} = store;
-
-const update = () => {
-  document.querySelector('#counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   return dispatch(creator(...args));
-// }
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-
-document.querySelector('#dec').addEventListener('click', inc)
-document.querySelector('#inc').addEventListener('click', dec)
-document.querySelector('#rnd').addEventListener('click', ()=> {
-  const value =  Math.floor(Math.random() * 10 + 1);
-  rnd(value);
-})
-
-
-
-
-
-
-
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <>
-
-  </>
+  <Provider store={store}>
+    <App/>
+  </Provider>
 );
-
-
-
